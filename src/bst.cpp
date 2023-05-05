cff951a1a2ce633133d2b9ec2970f8a9eee7c2cd
Rexiao <42951357+Rexiao@users.noreply.github.com>
2023-05-05 18:25:01 -0500

    switch

--- src/bst.cpp
+++ src/bst.cpp
@@ -1,5 +1,9 @@
 #include "bst.h"
 
+#include <compare>
+#include <functional>
+#include <iostream>
+
 BST::Node::Node(int value, Node* left, Node* right)
     : value(value), left(left), right(right) {}
 
@@ -7,3 +11,7 @@ BST::Node::Node() : value(0), left(nullptr), right(nullptr) {}
 
 BST::Node::Node(const Node& node)
     : value(node.value), left(node.left), right(node.right) {}
+
+std::strong_ordering BST::Node::operator<=>(const int& other_val) {
+    return value <=> other_val;
+}
